e9abaf465d2383560b40433e9ab3d14521a57fe6
emilys314 <emilys314@protonmail.com>
2023-05-26 18:54:10 -0400

    invoke simple rust command

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { invoke } from '@tauri-apps/api';
+
+invoke('greet', { name: 'World ' })
+  .then((response) => console.log(response));
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

b7448cedce3baf9d68404d48facd38e6387efa2d
emilys314 <emilys314@protonmail.com>
2023-06-04 11:45:46 -0400

    Create / use rust wasm npm package

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { invoke } from '@tauri-apps/api';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { invoke } from "@tauri-apps/api";
 
-invoke('greet', { name: 'World ' })
-  .then((response) => console.log(response));
+invoke("greet", { name: "World " }).then((response) => console.log(response));
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import init, { greet } from "../hello-wasm/pkg/hello_wasm.js";
+init().then(() => {
+  greet("WebAssembly");
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
